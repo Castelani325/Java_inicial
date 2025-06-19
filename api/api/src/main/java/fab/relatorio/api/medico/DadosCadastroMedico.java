c90d9f64603fb37fded81f8d421bdf5d5d96be4a
Otavio Vidal <otavio.castelani@gmail.com>
2025-06-19 00:01:04 -0400

    Acrescentado um Post cadastrar com suas classes que permitem ler um JSON.

---
+++ api/api/src/main/java/fab/relatorio/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,4 @@
+package fab.relatorio.api.medico;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
---
+++ api/api/src/main/java/fab/relatorio/api/medico/Especialidade.java
@@ -0,0 +1,8 @@
+package fab.relatorio.api.medico;
+
+public enum Especialidade {
+    ortopedia,
+    cardiologia,
+    ginecologia,
+    dermatologia;
+}
