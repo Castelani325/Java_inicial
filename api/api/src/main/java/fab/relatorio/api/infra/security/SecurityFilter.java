144b713567d375b73d4efb4abe9ab730d9127afb
Otavio Vidal <otavio.castelani@gmail.com>
2025-07-16 20:59:19 -0400

    Feita a requisição de token pela classe SecurityFIlter.

---
+++ api/api/src/main/java/fab/relatorio/api/infra/security/SecurityFilter.java
@@ -0,0 +1,37 @@
+package fab.relatorio.api.infra.security;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.Null;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //O token precisa ser enviado no header Authorization no front end
+        var tokenJWT = recuperarToken(request);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null ) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+        }
+
+        return authorizationHeader.replace ("Bearer", "");
+    }
+
+}
