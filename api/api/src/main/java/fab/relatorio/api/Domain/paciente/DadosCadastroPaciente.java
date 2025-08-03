097c7f09529b7a82308bd63a222d6bca58727acf
Otavio Vidal <otavio.castelani@gmail.com>
2025-08-03 17:28:10 -0400

    Implementado dois testes iniciais (Cenario01 e Cenario02).

--- api/api/src/main/java/fab/relatorio/api/Domain/paciente/DadosCadastroPaciente.java
+++ api/api/src/main/java/fab/relatorio/api/Domain/paciente/DadosCadastroPaciente.java
@@ -3,23 +3,19 @@ package fab.relatorio.api.Domain.paciente;
 
 
 import fab.relatorio.api.Domain.endereco.DadosEndereco;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 
 public record DadosCadastroPaciente (
-    @NotBlank String nome,
-    @NotBlank @Email String email,
-    @NotBlank String telefone,
-    @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "CPF inválido") String cpf,
+        @NotBlank String nome,
+        @NotBlank @Email String email,
+        @NotBlank String telefone,
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "CPF inválido") String cpf,
 
 
-    @NotNull
-    @Valid
-    DadosEndereco endereco
+        DadosEndereco endereco
 
 
 ){}
