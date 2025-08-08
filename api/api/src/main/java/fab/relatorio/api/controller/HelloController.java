348878df905f5c22955bdedc0f88ab756cbf0905
Otavio Vidal <otavio.castelani@gmail.com>
2025-08-07 22:05:34 -0400

    Acrescentado anotações sobre a execução do arquivo .jar e como acessar a documentação pela URL do Swagger no HelloController.

--- api/api/src/main/java/fab/relatorio/api/controller/HelloController.java
+++ api/api/src/main/java/fab/relatorio/api/controller/HelloController.java
@@ -13,4 +13,12 @@ public class HelloController {
     public String olaMundo(){
         return "Hello World, Spring!";
     }
+
+
+    //Para rodar o arquivo .jar que faz o servido backend funcionar, use o comando abaixo.
+    //& "C:\Users\Otavi\.jdks\openjdk-24.0.1\bin\java.exe" "-Dspring.profiles.active=prod" "-DDATASOURCE_URL=jdbc:mysql://localhost/Alura_api" "-DDATASOURCE_USERNAME=root" "-DDATASOURCE_PASSWORD=root" -jar "D:\OneDrive\Área de Trabalho\Java_curso\api\api\target\api-0.0.1-SNAPSHOT.jar"
+
+
+    //Use a URl abaixo para acessar a documentacao
+    //http://localhost:8080/swagger-ui/index.html#/
 }
